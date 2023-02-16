13b08315ec857d28397f16433716d0586a7e9ec4
Ahmad N <naimi.airsh@gmail.com>
2023-02-16 12:21:09 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -56,7 +56,4 @@ app.post('/send-email', (req, res) => {
     })
 })
 
-const port = process.env.PORT || 4000
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+app.listen(process.env.PORT||3000)
